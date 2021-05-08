5df8fe7f8428248bd594ba122ac069f700b6cbb9
Nasrul Gunawan <nasrul.gunawan@nutrifood.co.id>
2021-05-08 14:12:10 +0700

    Add dist

---
+++ dist/javascripts/app.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../stylesheets/app.css';
+import UI from './ui.js';
+import logo from '../images/logo.svg';
+
+document.querySelector('.navbar-brand img').src = logo;
+
+UI.setHamburgerMenu();
+UI.renderRestaurantMenu();
